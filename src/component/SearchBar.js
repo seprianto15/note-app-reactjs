5c8a3cc59595072912843e89bd62bc0853b7e113
Seprianto Sianipar <raysianipar15@gmail.com>
2022-07-19 22:35:36 +0700

    update

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const SearchBar = ( {onSearch} ) => {
+const SearchBar = ( {onSearch, searchTitle} ) => {
         return (
             <div className='note-app__header'>
                 <h1>Note App</h1>
                 <div className='note-search'>
-                    <input type='text' placeholder='Cari catatan ...' onChange={onSearch} />
+                    <input type='text' placeholder='Cari catatan ...' onChange={onSearch} value={searchTitle}/>
                 </div>
             </div>
         )
